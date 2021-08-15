eda21640259892ca4ff0272a3a07a49f6377eb53
unknown <shubhamdudhal9@gmail.com>
2021-08-15 19:52:34 +0530

    final update

---
+++ assets/js/new-age.js
@@ -0,0 +1,238 @@
+(function() {
+  "use strict"; // Start of use strict
+
+  var mainNav = document.querySelector('#mainNav');
+
+  if (mainNav) {
+
+    var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    
+    if (navbarCollapse) {
+      
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      
+      var navbarItems = navbarCollapse.querySelectorAll('a');
+      
+      // Closes responsive menu when a scroll trigger link is clicked
+      for (var item of navbarItems) {
+        item.addEventListener('click', function (event) {
+          collapse.hide();
+        });
+      }
+    }
+
+    // Collapse Navbar
+    var collapseNavbar = function() {
+
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop > 100) {
+        mainNav.classList.add("navbar-shrink");
+      } else {
+        mainNav.classList.remove("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    collapseNavbar();
+    // Collapse the navbar when page is scrolled
+    document.addEventListener("scroll", collapseNavbar);
+  }
+
+})(); // End of use strict
+
+
+$(function() {
+
+	if ( $('.owl-2').length > 0 ) {
+        $('.owl-2').owlCarousel({
+            center: false,
+            items: 1,
+            loop: true,
+            stagePadding: 0,
+            margin: 20,
+            smartSpeed: 1000,
+            autoplay: true,
+            nav: true,
+            dots: true,
+            pauseOnHover: false,
+            responsive:{
+                600:{
+                    margin: 20,
+                    nav: true,
+                  items: 2
+                },
+                1000:{
+                    margin: 20,
+                    stagePadding: 0,
+                    nav: true,
+                  items: 3
+                }
+            }
+        });            
+    }
+
+})
+
+
+
+!(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
+
+
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop: true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
+
+
+
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:true,
+	    dots: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
+
+
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
